d790d5dafc9f2d7a61fe1dce2941e61c0f0e003c
Aramis2424 <srd2424@yandex.ru>
2024-09-16 23:14:41 +0300

    add notification

---
+++ src/components/notification.js
@@ -0,0 +1,18 @@
+export const requestNotificationPermission = () => {
+    if ("Notification" in window) {
+        Notification.requestPermission().then((permission) => {
+            if (permission !== "granted") {
+                alert("Уведомления отключены пользователем.")
+            }
+        })
+    }
+}
+
+export const showNotification = () => {
+  if (Notification.permission === "granted") {
+    new Notification("Таймер завершен", {
+      body: `Помидор`,
+      icon: "https://cdn-icons-png.flaticon.com/512/992/992700.png", // Иконка уведомления
+    })
+  }
+}
